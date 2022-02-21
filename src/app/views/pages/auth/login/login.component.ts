16d562c506ffb175f3eac64dea6d79ef3a31a587
Michael <lider.desarrollo@sycgroup.co>
2022-02-21 09:21:48 -0500

    se agrega alert toast en login

--- src/app/views/pages/auth/login/login.component.ts
+++ src/app/views/pages/auth/login/login.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
+import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
 import { finalize } from 'rxjs';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { LocalStorageService } from 'src/app/core/services/local-storage.service';
+import { ToastServiceService } from 'src/app/core/services/toast-service.service';
 
 @Component({
   selector: 'app-login',
@@ -16,12 +18,15 @@ export class LoginComponent implements OnInit {
   loginForm : FormGroup;
   isLoading:boolean;
 
+  @ViewChild('ngAlert') ngAlert : NgbAlert;
+  
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private formBuilder: FormBuilder,
     private authService : AuthService,
-    private localService : LocalStorageService
+    private localService : LocalStorageService,
+    private toastService : ToastServiceService
   ) {
     this.loginForm = this.formBuilder.group({
       email : ['',[Validators.required,Validators.email]],
@@ -34,7 +39,12 @@ export class LoginComponent implements OnInit {
 
   login(){
     this.isLoading = true;
-    this.authService.login(this.loginForm.value).subscribe({
+    this.authService.login(this.loginForm.value).pipe(
+      finalize( () => {
+        this.loginForm.reset();
+        this.isLoading = false;
+      } )
+    ).subscribe({
       next: (resp) => {
         this.localService.setToken(resp);
         if(resp.access_token){ 
@@ -42,11 +52,12 @@ export class LoginComponent implements OnInit {
         }
       },
       error : (error) => {
-        alert(error.error?.message ?? "Fallo de Autenticación");
-      },
-      complete : () => {
-        this.loginForm.reset();
         this.isLoading = false;
+        this.toastService.show(error.error.message, {
+          classname: 'bg-danger text-light',
+          delay: 3000 ,
+          autohide: true
+        });
       }
     })
   }
