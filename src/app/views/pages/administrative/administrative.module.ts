c30dfb9c6adb14a51f9ff5a652dabcd0debc6e78
Victor <aprendizdesarrollo2@sycgroup.co>
2022-02-24 08:06:36 -0500

    dev

--- src/app/views/pages/administrative/administrative.module.ts
+++ src/app/views/pages/administrative/administrative.module.ts
@@ -5,12 +5,12 @@ import { AdministrativeRoutingModule } from './administrative-routing.module';
 import { EmployeesComponent } from './employees/employees.component';
 import { ManageVacationsComponent } from './manage-vacations/manage-vacations.component';
 import {NgbPaginationModule, NgbAlertModule, NgbNavModule} from '@ng-bootstrap/ng-bootstrap';
-import { InfoUserComponent } from './employees/components/info-user/info-user.component';
 import { AdminCovenantComponent } from './covenants/admin-covenant/admin-covenant.component';
 import { AssingCovenantComponent } from './covenants/assing-covenant/assing-covenant.component';
 import { SliderComponent } from './covenants/slider/slider.component';
 import { SwiperModule } from 'swiper/angular';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { InfoUserComponent } from './employees/components/info-user/info-user.component';
 
 
 
