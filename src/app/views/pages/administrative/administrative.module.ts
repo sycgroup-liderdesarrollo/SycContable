0cb02c8f17bfe1f73a06ddb2c4f6182e025c9b3a
Victor <aprendizdesarrollo2@sycgroup.co>
2022-03-07 08:52:46 -0500

    se modifica modal add employees organizando estructura numero de identificacion

--- src/app/views/pages/administrative/administrative.module.ts
+++ src/app/views/pages/administrative/administrative.module.ts
@@ -16,8 +16,6 @@ import { ConfirmationModalComponent } from './covenants/components/confirmation-
 import { PipefilterPipe } from './pipes/pipefilter.pipe';
 import { ModalAddEmployeesComponent } from './employees/components/modal-add-employees/modal-add-employees.component';
 // modulos material
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatInputModule} from '@angular/material/input';
 import { EmployeesComponent } from './employees/components/table-employee/employees.component';
 
 @NgModule({
@@ -43,12 +41,6 @@ import { EmployeesComponent } from './employees/components/table-employee/employ
     NgbPaginationModule,
     NgbAlertModule,
     NgbNavModule,
-    MatDialogModule,
-    MatInputModule,
-    MatStepperModule,
-    MatButtonModule,
-    MatProgressSpinnerModule,
-    MatCardModule
   ]
 })
 export class AdministrativeModule { }
